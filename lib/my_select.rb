8195741cc1ea82ecfe71f6750a9b361d7a021019
Alex Merecka <merecka@gmail.com>
2019-02-24 04:53:40 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,7 @@ def my_select(collection)
  new_array = []
  i = 0
  while i < collection.length
-  if (yield collection[i]) == true
+  if yield collection[i] == true
  ## if (yield collection[i])
     new_array[i] << collection[i]
   else
